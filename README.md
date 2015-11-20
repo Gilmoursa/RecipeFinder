01f4f16ab4b7b971fbdec56760092badc36a5b20
Gilmoursa <gilmoursa@gmail.com>
2015-11-20 13:25:27 -0500

    switching to yummly api

--- README.md
+++ README.md
@@ -8,9 +8,19 @@ Use the Yummly API to find recipes and display them in a grid. A simple practice
 
 Bootstrap with minor CSS modifications
 
+## Phase One
+
+- Sort out the Yummly API call
+- Search for recipes
+	- display list of results with title, image, and link to source
+
+## Phase Two
+
+- Create view page for each search result
+- display ingredients, link to source, title, and image
+
 ## Future Features
 
  1. Log In 
  2. Add Favorites
- 3. Display Favorites
- 4. Share with URL shortener
+ 3. Display Favorites ( same as search result view page)
--- js/yummlySearch.js
+++ js/yummlySearch.js
@@ -1,28 +1,37 @@
 $(document).ready(function(){
-    $('#search').click(function(e){ 
-      e.preventDefault(); 
-      clearFeed();
-      var keyword = getKeyword();
-      debugger;
-      $.ajax({
-        url: "http://food2fork.com/api/search",
-        type: "GET",
-        data: { q: keyword, key: '4f5a06d7e318f0826d16847dec546eb3' },
-        dataType: "JSON"
-      }).done(function(yumData){
-        debugger;
-            yumData.recipes.forEach(function(recipe){
-                $('#feed').append(recipe.publisher);
-            });
-      }); 
-    })
+  $('#search').click(function(e){ 
+    e.preventDefault(); 
+    clearFeed();
+    var keyword = getKeyword();
+    var link = getLink(keyword);
+    $.ajax({
+      url: link,
+      type: "GET",
+      dataType: "JSON"
+    }).success(function(yumData){
+      yumData.matches.forEach(function(recipe){
+        generateThumbnailFor(recipe);
+      });
+    }); 
+  })
  });
   function clearFeed(){
     $('#feed').html(' ');
   };
+  function getLink(keyword){
+    return 'http://api.yummly.com/v1/api/recipes?_app_id=e117b611&_app_key=41a3c4bb5d793a1d5733392e96218eab&q=' + keyword;
+  };
   function getKeyword(){
     return $('#keyword').val();
   };
-  // function getImageTag(image){
-  //   $('#feed').append(image_tag);
-  // };
+  function generateThumbnailFor(recipe){
+    var imageUrl = recipe.imageUrlsBySize[90]; 
+    var title = recipe.sourceDisplayName;
+    var id = recipe.id;
+    var imageTag =  "<img src='" + imageUrl +"'>";
+    var yumUrl = "http://www.yummly.com/recipe/" + id +"";
+    var recipeThumbnail = "<div class='col-sm-6 col-md-4'><div class='thumbnail'>" + imageTag + "<div class='caption'><a href='"+ yumUrl +  "'><h4>" + title + "</h4></a></div></div></div>"
+    $('#feed').append(recipeThumbnail);
+  };
+
+  //http://api.yummly.com/v1/api/recipe/French-Onion-Soup-The-Pioneer-Woman-Cooks-_-Ree-Drummond-41364?_app_id=e117b611&_app_key=41a3c4bb5d793a1d5733392e96218eab
